6de9acf6156875e8807bfda95781a31b5c4c1ede
BattinaAnusha <94469753+BattinaAnusha@users.noreply.github.com>
2023-06-08 17:08:25 +0530

    Update dockerrun.sh
    first change

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,3 +1,3 @@
-sudo docker pull yedukondalu212/java-app-demo:latest
+sudo docker pull anushabattina/java-app-demo:latest
 sudo docker rm -f javaapp
-sudo docker run -itd -p 8004:8080 --name javaapp yedukondalu212/java-app-demo:latest
+sudo docker run -itd -p 8004:8080 --name javaapp anushabattina/java-app-demo:latest
