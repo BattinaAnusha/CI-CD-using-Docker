aab150caa590170c4a17dbf07e22861aeb915096
BattinaAnusha <94469753+BattinaAnusha@users.noreply.github.com>
2023-06-09 13:24:10 +0530

    Update dockerpush.sh

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -1,6 +1,6 @@
 
 #sudo docker login --username=$username --password=$password
-sudo docker logout
+#sudo docker logout
 #echo $password >> pass.txt
 sudo docker login --username anushabattina --password-stdin
 sudo docker push anushabattina/java-app-demo:latest
